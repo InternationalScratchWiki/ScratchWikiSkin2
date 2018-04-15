e16311451bc9fb5135c48a0c3e091b54d43e08a1
apple502j <33279053+apple502j@users.noreply.github.com>
2018-04-15 10:33:52 +0900

    Added Colon Checker
    This is used only in enwiki until ST changes the wiki tag in forums

--- ScratchWikiSkinTemplate.php
+++ ScratchWikiSkinTemplate.php
@@ -290,6 +290,18 @@ window.addEventListener('load', function(){
 window.addEventListener('hashchange', function(){
         window.scrollBy(0, -50);
 });
+function checkColon(e){
+    var nameColon = decodeURIComponent(document.URL);
+    if (document.domain != "en.scratch-wiki.info" || mw.config.get("wgPageName") != "Special:Search"){
+        return;
+    }
+    if (nameColon.toLowerCase().indexOf("%3a") > -1){
+        nameColon = nameColon.replace("%3A",":").replace("%3a",":");
+        location.href = nameColon;
+    }
+}
+
+window.addEventListener("load",checkColon);
 </script>
 <?php $this->printTrail();
 	}
