a2035bfce83b10fa1651289f06e0aa8aced53832
Kenny2github <kenny2minecraft@gmail.com>
2018-04-29 22:04:36 +0800

    Don't wait until page is finished loading - the script is at the end anyway

--- ScratchWikiSkinTemplate.php
+++ ScratchWikiSkinTemplate.php
@@ -232,69 +232,71 @@ ScratchWikiSkin script
 
 //get an element from a query selector, with functions to write, add, show, hide, addclass, delclass
 function mod(el) {
-        if (!el) return el;
-        el.addclass = function(c) {this.classList.add(c);};
-        el.delclass = function(c) {this.classList.remove(c);};
-        el.hasclass = function(c) {return this.classList.contains(c);};
+	if (!el) return el;
+	el.addclass = function(c) {this.classList.add(c);};
+	el.delclass = function(c) {this.classList.remove(c);};
+	el.hasclass = function(c) {return this.classList.contains(c);};
 }
 
-window.addEventListener('load', function(){
-	(function(){
-		var nameColon = decodeURIComponent(document.URL);
-		if (document.domain != "en.scratch-wiki.info" || mw.config.get("wgPageName") != "Special:Search") return;
-		if (nameColon.toLowerCase().indexOf("%3a") > -1) {
-			nameColon = nameColon.replace("%3A", ":").replace("%3a", ":");
-			window.location.href = nameColon;
-		}
-	})()
-        for (let btn of document.querySelectorAll('#navigation a.dropdown-toggle')) {
-                let dropdown = btn.nextElementSibling;
-                mod(btn);
-                mod(dropdown);
-                btn.onclick = function(){
-                        if (!dropdown.classList.contains('open')) {
-                                btn.addclass('open');
-                                dropdown.addclass('open');
-                        } else {
-                                btn.delclass('open');
-                                dropdown.delclass('open');
-                        }
-                };
-                btn.parentElement.onmouseout = function(e) {
-                        if (!e) e = window.event;
-                        if (!e.toElement) e.toElement = e.relatedTarget;
-                        if (!e.toElement) return;
-                        if (!btn.parentElement.contains(e.toElement)) {
-                                if (btn.hasclass('open')) {
-                                        if (e.toElement.matches('#navigation .link, #navigation .link>a, #navigation .link>a *')) {
-                                                e.toElement.click();
-                                        }
-                                }
-                                btn.delclass('open');
-                                dropdown.delclass('open');
-                        }
-                };
-        };
-        if (document.querySelector(':target') !== null) {
-                window.scrollBy(0, -50);
-        };
-        document.querySelector('#searchInput').onfocus = function () {
-                for (let link of document.querySelectorAll('#navigation .link')) {
-                        if (!link.classList.contains('right')) {
-                                link.style.display = 'none';
-                        }
-                }
-        };
-        document.querySelector('#searchInput').onblur = function () {
-                for (let link of document.querySelectorAll('#navigation .link')) {
-                        if (!link.classList.contains('right')) {
-                                link.style.display = '';
-                        }
-                }
-        };
+(function(){
+	var nameColon = decodeURIComponent(document.URL);
+	if (document.domain != "en.scratch-wiki.info" || mw.config.get("wgPageName") != "Special:Search") return;
+	if (nameColon.toLowerCase().indexOf("%3a") > -1) {
+		nameColon = nameColon.replace("%3A", ":").replace("%3a", ":");
+		window.location.href = nameColon;
+	}
+})();
+(function () {
+	for (let btn of document.querySelectorAll('#navigation a.dropdown-toggle')) {
+		let dropdown = btn.nextElementSibling;
+		mod(btn);
+		mod(dropdown);
+		btn.onclick = function(){
+			if (!dropdown.classList.contains('open')) {
+				btn.addclass('open');
+				dropdown.addclass('open');
+			} else {
+				btn.delclass('open');
+				dropdown.delclass('open');
+			}
+		};
+		btn.parentElement.onmouseout = function(e) {
+			if (!e) e = window.event;
+			if (!e.toElement) e.toElement = e.relatedTarget;
+			if (!e.toElement) return;
+			if (!btn.parentElement.contains(e.toElement)) {
+				if (btn.hasclass('open')) {
+					if (e.toElement.matches('#navigation .link, #navigation .link>a, #navigation .link>a *')) {
+						e.toElement.click();
+					}
+				}
+				btn.delclass('open');
+				dropdown.delclass('open');
+			}
+		};
+	};
+})();
+window.addEventListener('load', function (){
+	if (document.querySelector(':target') !== null) {
+		window.scrollBy(0, -50);
+	};
 });
+document.querySelector('#searchInput').onfocus = function () {
+	for (let link of document.querySelectorAll('#navigation .link')) {
+		if (!link.classList.contains('right')) {
+			link.style.display = 'none';
+		}
+	}
+};
+document.querySelector('#searchInput').onblur = function () {
+	for (let link of document.querySelectorAll('#navigation .link')) {
+		if (!link.classList.contains('right')) {
+			link.style.display = '';
+		}
+	}
+};
 window.addEventListener('hashchange', function(){
-        window.scrollBy(0, -50);
+	window.scrollBy(0, -50);
 });
 </script>
 <?php $this->printTrail();
