a44dec365f307ce2ec67ccdaaa2f38e60ed11c2c
ahmetlii <ahmetlii@users.noreply.github.com>
2021-01-03 22:18:15 +0300

    Update ScratchWikiSkin.skin.php

--- ScratchWikiSkin.skin.php
+++ ScratchWikiSkin.skin.php
@@ -55,9 +55,7 @@ class SkinScratchWikiSkin extends SkinTemplate {
 	 */
 	public function initPage( OutputPage $out ) {
 		parent::initPage( $out );
-		$out->addModules( [
-			'mediawiki.skinning.interface.js', 'skins.scratchwikiskin2.js'
-		] );
+		$out->addModules('skins.scratchwikiskin2.js');
 		// make Chrome mobile testing work
 		$out->addMeta('viewport', 'user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width, height=device-height');
 	}
